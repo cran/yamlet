7ec86d4bfd9fbb9634aed395e2d88a8d3c964219
Tim Bergsma <bergsmat@gmail.com>
2022-11-24 02:20:02 +0000

    version 0.10.0

--- R/vec_decorated.R
+++ R/vec_decorated.R
@@ -1,9 +1,11 @@
 # https://vctrs.r-lib.org/reference/howto-faq-coercion-data-frame.html
 
+
 #' Coerce Common Type to Decorated
 #' 
 #' Coerces common type to decorated.  Wrapper for \code{\link[vctrs]{df_ptype2}}.
 #' @export
+#' @keywords internal
 #' @return decorated
 #' @param x subclass of data.frame
 #' @param y subclass of data.frame
@@ -19,6 +21,7 @@ dd_ptype2 <- function(x, y, ...) {
 #' 
 #' Coerces data.frame to decorated.  Wrapper for \code{\link[vctrs]{df_cast}}.
 #' @export
+#' @keywords internal
 #' @return decorated
 #' @param x subclass of data.frame
 #' @param to subclass of data.frame
@@ -31,47 +34,51 @@ dd_cast <- function(x, to, ...) {
 
 #' Determine Common Type for Decorated
 #' 
-#' Determines common type for decorated
+#' Determines common type for decorated.
 #' @return decorated
 #' @param x decorated
 #' @param y decorated
 #' @param ... passed arguments
 #' @export
+#' @keywords internal
 vec_ptype2.decorated.decorated <- function(x, y, ...) {
   dd_ptype2(x, y, ...)
 }
 
 #' Determine Common Type for Decorated and Data.frame
 #' 
-#' Determines common type for decorated and data.frame
+#' Determines common type for decorated and data.frame.
 #' @return decorated
 #' @param x decorated
 #' @param y data.frame
 #' @param ... passed arguments
 #' @export
+#' @keywords internal
 vec_ptype2.decorated.data.frame <- function(x, y, ...) {
   dd_ptype2(x, y, ...)
 }
-#' Determine Common Type for  Data.frame and decorated
+#' Determine Common Type for Data.frame and Decorated
 #' 
-#' Determines common type for data.frame and decorated
+#' Determines common type for data.frame and decorated.
 #' @return decorated
 #' @param x data.frame
 #' @param y decorated
 #' @param ... passed arguments
 #' @export
+#' @keywords internal
 vec_ptype2.data.frame.decorated <- function(x, y, ...) {
   dd_ptype2(x, y, ...)
 }
 
 #' Cast to Decorated from Decorated
 #' 
-#' Casts to decorated from decorated
+#' Casts to decorated from decorated.
 #' @export
 #' @return decorated
 #' @param x decorated
 #' @param to decorated
 #' @param ... passed arguments
+#' @keywords internal
 vec_cast.decorated.decorated <- function(x, to, ...) {
   dd_cast(x, to, ...)
 }
@@ -83,6 +90,7 @@ vec_cast.decorated.decorated <- function(x, to, ...) {
 #' @param x data.frame
 #' @param to decorated
 #' @param ... passed arguments
+#' @keywords internal
 vec_cast.decorated.data.frame <- function(x, to, ...) {
   # `x` is a data.frame to be converted to a decorated
   dd_cast(x, to, ...)
@@ -95,6 +103,7 @@ vec_cast.decorated.data.frame <- function(x, to, ...) {
 #' @param x decorated
 #' @param to data.frame
 #' @param ... passed arguments
+#' @keywords internal
 vec_cast.data.frame.decorated <- function(x, to, ...) {
   # `x` is a decorated to be converted to a data.frame
   df_cast(x, to, ...)
